8d5beb2b34027d7687f6897eec238e61f0764796
Diogo Malfatti <diimalfatti@gmail.com>
2022-02-19 20:52:17 -0300

    upload aulas5-0 desafio guiado jogo da velha

---
+++ 02Modulo-Angular/aulas/5-0-DesafioGuiado01-JogoDaVelha/jogoDaVelha/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TicTacToeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
